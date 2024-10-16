e99c5997df6561681933c601389973ce9c866436
Rika <melty.dupre@gmail.com>
2024-10-16 10:54:47 -0400

    Modification accepted.

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const cart = () => {
-<<<<<<< HEAD
   return <div>ここでかえられるよ</div>;
-=======
-  return <div>カート</div>;
->>>>>>> 8a443ede1307b57c86b31cab95ec2d39c58f0271
 };
 
 export default cart;
