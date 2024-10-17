057c70adf833955b8534877f6a561b346787d016
Rika <melty.dupre@gmail.com>
2024-10-17 08:48:42 -0400

    Added input section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/navigation-menu";
 import { Button } from "@/components/ui/button";
 import { buttonVariants } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Link from "next/link";
 
 export default function Home() {
@@ -28,9 +29,8 @@ export default function Home() {
         </NavigationMenuList>
       </NavigationMenu>
       <div>
+        <Input type="code" placeholder="バーコード" />
         <Button className={buttonVariants({ variant: "outline" })}>追加</Button>
-        <p>こんにちは！</p>
-        <p>Welcome to the POS app home page!</p>
       </div>
     </div>
   );
